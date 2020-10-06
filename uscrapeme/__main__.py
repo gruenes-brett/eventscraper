05cf217592ed0fe4af637f599b53060372cb87b4
Jörg <jeremyjove@gmail.com>
2020-10-06 22:27:10 +0200

    verbose output

--- uscrapeme/__main__.py
+++ uscrapeme/__main__.py
@@ -14,11 +14,18 @@ def main():
     if os.path.isfile(RESULT_FILE):
         os.remove(RESULT_FILE)
 
+    # command line arguments
     parser = argparse.ArgumentParser(description='Tool for scraping event information from websites. '
                                      'Currently supported: Facebook')
     parser.add_argument('url', help='URL of the event')
+    parser.add_argument('-v', action='store_true', default=False, help='More logging output')
     args = parser.parse_args()
     url = args.url
+    verbose = args.v
+    if verbose:
+        log.setLevel(logging.DEBUG)
+
+    # scrape data from url
     result = Scraper.scrape(url)
     log.info(f'result: {result}')
     with open(RESULT_FILE, 'w') as f:
