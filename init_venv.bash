21fbfda74be20276b08571d4dbe8cc923fae472f
Jörg <jeremyjove@gmail.com>
2020-10-11 21:55:07 +0200

    helper for installing as apache2 vhost

---
+++ init_venv.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export PIPENV_VENV_IN_PROJECT=1
+python3 -m pipenv sync
---
+++ install_apache2_vhost.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PROJECT_DIR=$(pwd)
+
+echo "VirtualHost will be installed to point to $PROJECT_DIR."
+
+echo "Please enter the port where the service should be accessible on:"
+read HTTP_PORT
+
+VHOST_FILE=$PROJECT_DIR/event-scraper-vhost.conf
+cat event-scraper-vhost-template.conf | sed s~XpathX~$PROJECT_DIR~g | sed s~XportX~$HTTP_PORT~g > $VHOST_FILE
+echo "$VHOST_FILE written"
+
+TARGET=/etc/apache2/conf-enabled/event-scraper-vhost.conf
+echo "Linking $TARGET to $VHOST_FILE"
+if test -h $TARGET; then
+  sudo rm $TARGET
+fi
+sudo ln -s -t /etc/apache2/conf-enabled $VHOST_FILE
+
+echo "Reloading apache2"
+sudo service apache2 reload
+echo "OK"
