4ea37005c8d197806ccdd79efc709790ed4af282
Jörg <jeremyjove@gmail.com>
2020-10-11 13:58:49 +0200

    Flask app: API-Endpoint /api/scrape?url=http...

---
+++ uscrapeme/apiserver.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, abort, Response
+
+from uscrapeme.scraper import Scraper
+
+
+class ApiServer(Flask):
+
+    def __init__(self, *args, **kwargs):
+        Flask.__init__(self, *args, **kwargs)
+        self._setup_endpoints()
+
+    def _setup_endpoints(self):
+        @self.route('/api/scrape', methods=['GET'])
+        def scrape():
+            url = request.args.get('url', None)
+            if url is None:
+                abort(Response('Missing parameter: url', status=400))
+            try:
+                data = Scraper.scrape(url)
+            except Exception as e:
+                abort(Response(f'Could not get event data from {url}: {e}', status=500))
+            return jsonify({'url': url, 'data': data})
+
+
+if __name__ == '__main__':
+    server = ApiServer(__name__)
+    server.run(debug=False)
