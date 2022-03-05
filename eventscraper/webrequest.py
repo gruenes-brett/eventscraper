473648c8ff2b4f5d4a06f90c2c0c0f8fc55e5d52
joergrs <joergschroeter84@gmail.com>
2022-03-05 15:07:21 +0100

    use fake-useragent for more randomness

--- eventscraper/webrequest.py
+++ eventscraper/webrequest.py
@@ -1,5 +1,6 @@
-from random import randint
+import random
 
+from fake_useragent import UserAgent
 from requests import Session
 import logging
 log = logging.getLogger(__name__)
@@ -10,15 +11,15 @@ class Request:
     HEADERS = [
         {
             'accept-language': 'en',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
         },
         {
             'accept-language': 'en',
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.71',
+            # 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36 OPR/54.0.2952.71',
         },
         {
             'Accept-Language': 'en-US,en;q=0.5',
-            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
+            # 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
         }
     ]
 
@@ -28,12 +29,14 @@ class Request:
     def get_response(self, payload=None):
         session = Session()
         session.headers.update(self._get_random_headers())
+        log.info(f'Headers: {session.headers}')
         response = session.get(self._url, data=payload)
         if response.status_code != 200:
             raise ValueError(f'Bad response code: {response.status_code}')
         return response.text
 
     def _get_random_headers(self):
-        headers = self.HEADERS[randint(0, len(self.HEADERS) - 1)].copy()
-        log.debug(f'Headers: {headers}')
+        ua = UserAgent(num_newest_uas=10)
+        headers = random.choice(self.HEADERS).copy()
+        headers['User-Agent'] = ua.firefox
         return headers
--- init_venv.bash
+++ init_venv.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm -R venv
 python3 -m virtualenv venv
 venv/bin/python -m pip install -r requirements.txt
